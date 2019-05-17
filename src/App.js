d7c6659b9bba95847bccf7224f8f4be6c171cae6
Tzikas <niko.tzikas@gmail.com>
2019-05-17 10:48:27 -0400

    front end react api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  state={
+    allCountries:[],
+    doggy:''
+  }
+  
+  componentDidMount(){
+    // Promise.all([
+    //   axios.get(`http://206.189.7.127/countries`),
+    //   axios.get(`http://localhost:3000`),
+    //   axios.get(`http://192.168.125.100:5000`),
+    //   axios.get(`https://dog.ceo/api/breeds/image/random`)
+    // ]).then(responses=>{
+    //   console.log(responses)
+    //   this.setState({doggy: responses[3].data.message})
+    // })
+    axios.get(`http://206.189.7.127/countries`).then(allCountries=>{
+      this.setState({allCountries:allCountries.data})
+    })
+
+    axios.get(`http://localhost:3000`).then(myData=>{
+      console.log('from localhost', myData.data)
+    })
+
+    axios.get(`http://192.168.125.100:5000`).then(chrisData => {
+      console.log('from chris', chrisData.data)
+    })
+
+    axios.get(`https://dog.ceo/api/breeds/image/random`).then(giffy=>{
+        console.log('from giphy', giffy.data)
+        this.setState({
+          doggy:giffy.data.message
+        })
+    })
+
+  }
+
+  showCountries = () => {
+    return this.state.allCountries.map(eachCountry=>{ 
+      return <li>{eachCountry.name.common}</li>
+    })
+  }
+
+  render() {
+    
+    return (
+      <div className="App">
+        React-Api
+        <img src={this.state.doggy} />
+
+        {this.showCountries()}
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
